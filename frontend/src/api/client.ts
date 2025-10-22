04acff66d09f64e5424e683f7691e333d9ce3419
FyliaCare <fyliacare@gmail.com>
2025-10-22 18:13:00 +0000

    Fix TypeScript errors for frontend build

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,14 +1,13 @@
-import axios from 'axios'
+import axios, { InternalAxiosRequestConfig } from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
 })
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem('token')
   if (token) {
-    config.headers = config.headers || {}
-    config.headers['Authorization'] = `Bearer ${token}`
+    config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
